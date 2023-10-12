89519f714816e71cf2ad52d3f3dba5ab01928706
andik.saputro <andik.saputro@pactindo.com>
2023-10-12 11:24:38 +0700

    add student service

---
+++ src/main/java/com/andikscript/springcrud/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.andikscript.springcrud.service;
+
+import com.andikscript.springcrud.dao.StudentDao;
+import com.andikscript.springcrud.dto.StudentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class StudentService {
+
+    @Autowired
+    private StudentDao studentDao;
+
+    public void setStudentDao(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+    public List<StudentDto> getAllStudent() {
+        return studentDao.getAllStudent();
+    }
+}
